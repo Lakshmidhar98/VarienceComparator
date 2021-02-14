914823cf5844d1303b220a962f961dbc561d8611
Lakshmidhar <lakshmidhar1998@gmail.com>
2021-02-14 12:58:12 +0530

    Logic is implemented

---
+++ logic.py
@@ -0,0 +1,28 @@
+
+
+file1 = [x.strip() for x in open("D:/file1.txt","r").readlines()]
+print(file1)
+file2 = [x.strip() for x in open("D:/file2.txt","r").readlines()]
+print(file2)
+matches = open("D:/some_out.txt","w")
+for x in file1:
+    bool = False
+
+    for y in file2:
+        if x == y:
+            bool = True
+    if bool == False:
+        print(x)
+        matches.write(x +'\n')
+
+for y in file2:
+    bool = False
+
+    for x in file1:
+        if x == y:
+            bool = True
+    if bool == False:
+        print(y)
+        matches.write(y + '\n')
+
+
