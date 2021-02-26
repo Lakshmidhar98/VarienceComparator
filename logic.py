18f694c5b1e22fed0933d687e71b67adc415222e
Lakshmidhar <lakshmidhar1998@gmail.com>
2021-02-26 23:14:00 +0530

    create a outputfile and added new validation

--- logic.py
+++ logic.py
@@ -25,23 +25,30 @@
 #         print(y)
 #         matches.write(y + '\n')
 # matches.close()
+
 import os
 
-def getDifferenceFrom(file1, file2, diffpath):
-    print(file1)
-    print(file2)
+def getDifferenceFrom(file1, file2):
+   # print(file1)
+    #print(file2)
+
+    if ((os.path.exists(file1)) == False) & ((os.path.exists(file2)) == False):
+       print('Files are not available at' + file1, file2)
+       return None
+
     if (os.path.exists(file1)) == False:
-        print('File is not available at '+file1)
+        print('File1 is not available at '+file1)
         return None
+
     if (os.path.exists(file2)) == False:
-        print('File is not available at ' + file2)
+        print('File2 is not available at ' + file2)
         return None
+
     data1 = [x.strip() for x in open(file1,"r").readlines()]
     data2 = [x.strip() for x in open(file2,"r").readlines()]
-    output_file = open(diffpath,"w")
+    diff_file = open("D:/some_out.txt", "w+")
     print(data1)
     print(data2)
-
 
 
     for x in data1:
@@ -52,43 +59,21 @@ def getDifferenceFrom(file1, file2, diffpath):
                 bool = False
         if bool == True:
             print(x)
-            output_file.write(x + '\n')
+            diff_file.write(x + '\n')
+            return diff_file
 
     for x in data2:
         bool = True
         for y in data1:
-           # print(x,'=>',y)
             if x == y:
                 bool = False
         if bool == True:
             print(x)
-            output_file.write(x + '\n')
+            diff_file.write(x + '\n')
+            return diff_file
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-getDifferenceFrom('D:/file12121.txt','D:/file2.txt','D:/some_out.txt')
-
-
-
-
-
-
-
-
-
-
+diffFile = getDifferenceFrom('D:/file1.txt','D:/file2.txt')
 
